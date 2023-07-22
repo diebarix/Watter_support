dcff2116138a1dfa9cdf2d9ec094d2fa6d1c9aa3
Guadalupe Gutierrez <jose.gutierrez1319@alumnos.udg.mx>
2023-07-21 19:28:19 -0600

    Add logged user restriction to pages
    Only logged users can access pages like home, create, nft, etc., with
    their token provided by the backend.
    Landing page is the only page accesible to everyone.

--- frontend/src/pages/index.tsx
+++ frontend/src/pages/index.tsx
@@ -3,18 +3,31 @@ import { Home } from "./home";
 import { Register } from "./register";
 import { Create } from "./create";
 import { NFT } from "./nft";
+import { Landing } from "./landing";
+import { Login } from "./login";
+import { useToken } from "../hooks";
 
 const routes = [
-	{ path: "/", Page: Home },
-	{ path: "/register", Page: Register },
-	{ path: "nft/:id", Page: NFT },
+	{ path: "/", Page: Landing, isPrivate: false },
+	{ path: "/home", Page: Home, isPrivate: true },
+	{ path: "/register", Page: Register, isPrivate: true },
+	{ path: "nft/:id", Page: NFT, isPrivate: true },
 	{ path: "/create", Page: Create, isPrivate: true },
 ];
 
+// Pages with isPrivate set to true need the existence of a
+// userToken in order to allow access, i. e. the user needs
+// to login with their account.
 function Routing() {
+    const { token, setToken } = useToken();
+
 	const getRoutes = () =>
 		routes.map(({ path, Page, isPrivate }) => (
-			<Route key={path} path={path} element={<Page />} />
+			<Route
+                key={path}
+                path={path}
+                element={(token === null && isPrivate) ? <Login setToken={setToken} /> : <Page />}
+            />
 		));
 
 	return <Routes>{getRoutes()}</Routes>;
---
+++ frontend/src/pages/landing/Landing.tsx
@@ -0,0 +1,7 @@
+function Landing() {
+    return (
+        <div>This is the landing page</div>
+   );
+}
+
+export { Landing };
